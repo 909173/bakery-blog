a66505b1d4d3e222ef71b84cc9ef4a220616d648
unknown <49011@ksc.local>
2019-05-21 18:32:06 +0900

    change page

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,9 @@
-import { configure } from "@storybook/react";
-
+import { configure } from '@storybook/react'
+const req = require.context('../src/components', true, /\.stories\.js$/)
 function loadStories() {
-  require("../stories/index.js");
+  require('../stories/index.js')
+  req.keys().forEach(filename => req(filename))
   // You can require as many stories as you need.
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './all.sass'
 
-const Logo = () => {
-  return <img src="../img/T.png" />
+export const Logo = () => {
+  return <img src="https://avatars2.githubusercontent.com/u/22632046?s=400&v=4" />
 }
